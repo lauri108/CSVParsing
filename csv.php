2bfdd314c53606fe1f32289446c8901380608cd5
Lauri Jalonen <tapani108@gmail.com>
2013-12-18 04:20:33 +0200

    removed UTF string formatting from CSV parser

--- csv.php
+++ csv.php
@@ -136,7 +136,8 @@ class CsvReader implements Iterator {
 	}
 
 	protected function _readLine() {
-		if (!feof($this->fileHandle)) $this->currentLine = trim(utf8_encode(fgets($this->fileHandle)));
+		//if (!feof($this->fileHandle)) $this->currentLine = trim(utf8_encode(fgets($this->fileHandle)));
+		if (!feof($this->fileHandle)) $this->currentLine = trim(fgets($this->fileHandle));
 		else $this->currentLine = null;
 		if ($this->currentLine != '') $this->currentArray = Csv::parseString($this->currentLine, $this->separator);
 		else $this->currentArray = null;
