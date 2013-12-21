a1efb5f8fa66e0f3887d0e0b853b0f2227ea2bc7
Lauri Jalonen <tapani108@gmail.com>
2013-12-21 21:21:54 +0200

    Changed the CSVReader name back accordingly, added try/catch for Restaurant creation

--- read-file.php
+++ read-file.php
@@ -1,15 +1,17 @@
 <?php
 
-// CSV parser from https://github.com/ockam/php-csv
+// CSV reader and parser from https://github.com/ockam/php-csv
 require_once('CSVParser.php');
+require_once('CSVReader.php');
 
+// Restaurant class
 require_once('Restaurant.php');
 
 setlocale(LC_TIME, "fi_FI");
 date_default_timezone_set('Europe/Helsinki');
 
-// Parse CSV
-$arrayOfRestaurants = new CSVParser(dirname(__FILE__).'/ravintolat.csv', $separator=";");
+// Read and parse CSV
+$arrayOfRestaurants = new CSVReader(dirname(__FILE__).'/ravintolat.csv', $separator=";");
 
 // Set vars for min and max opening times
 $leastOpeningHours = 10000;
@@ -18,31 +20,44 @@ $leastOpenRestaurantName = "";
 $mostOpeningHours = 0;
 $mostOpenRestaurantName = "";
 
+// This var is used for optional CSV output for testing purposes
 $csvstring = "";
 
-foreach ($arrayOfRestaurants as $key => $values) {
+foreach ($arrayOfRestaurants as $key => $currentRestaurantValues) {
 
 	// used for comparing the current Restaurant total hours to whole array min and max
 	$restaurantTotalWeeklyOpeningHours = 0;
 	
-	$currentRestaurant = new Restaurant($values);
- 	$restaurantTotalWeeklyOpeningHours = $currentRestaurant->getOpeningHoursPerWeekTotal();
-	$restaurantName = $currentRestaurant->getName();
+	try {
+		
+		// attempt to create a new Restaurant
+		$currentRestaurant = new Restaurant($currentRestaurantValues);
+			
+		// get the Restaurant information
+	 	$restaurantTotalWeeklyOpeningHours = $currentRestaurant->getOpeningHoursPerWeekTotal();
+		$restaurantName = $currentRestaurant->getName();
+		
+		// check whether current Restaurant has the most hours so far
+		if($restaurantTotalWeeklyOpeningHours > $mostOpeningHours){
+			
+			$mostOpeningHours = $restaurantTotalWeeklyOpeningHours;
+			$mostOpenRestaurantName = $currentRestaurant->getName();
+			
+		}
+		
+		// check whether current Restaurant has the least hours so far
+		if($restaurantTotalWeeklyOpeningHours < $leastOpeningHours){
+			
+			$leastOpeningHours = $restaurantTotalWeeklyOpeningHours;
+			$leastOpenRestaurantName = $currentRestaurant->getName();
+			
+		}
 	
-	if($restaurantTotalWeeklyOpeningHours > $mostOpeningHours){
+	} catch (Exception $e) {
 		
-		$mostOpeningHours = $restaurantTotalWeeklyOpeningHours;
-		$mostOpenRestaurantName = $currentRestaurant->getName();
-		
+    	echo 'There was an exception: ',  $e->getMessage(), "\n";
+	
 	}
-	
-	if($restaurantTotalWeeklyOpeningHours < $leastOpeningHours){
-		
-		$leastOpeningHours = $restaurantTotalWeeklyOpeningHours;
-		$leastOpenRestaurantName = $currentRestaurant->getName();
-		
-	}
-	
 	// output a CSV string with only the names and counts
 	// $csvstring .= $restaurantName . ";" . $hourCount . ";\n";
 }
